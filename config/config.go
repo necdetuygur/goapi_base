a6c6a796e1741747624c28d3436f31f7141cd5a6
Necdet UYGUR <necdet.uygur@gmail.com>
2022-01-30 19:09:06 +0300

    config

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+const DB_NAME = "./database.db"
+const PORT = "3543"
+const AUTH_JWT_TOKEN = "V3ryS3cr3T"
+const AUTH_TIMEOUT_HOUR = 72
+const AUTH_USERNAME = "Necdet"
+const AUTH_PASSWORD = "12345alti"
