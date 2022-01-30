73d45dced6b6e5fe59af830c18b5cc0d87dae7da
Necdet UYGUR <necdet.uygur@gmail.com>
2022-01-30 19:45:22 +0300

    main

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"goapi_base/config"
+	"goapi_base/model"
+	"goapi_base/router"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// VERİ TABANINDA TABLO YOKSA OLUŞTUR METODLARI
+	model.TodoCreateTable()
+
+	// WEB FRAMEWORK TANIMLAMALARI
+	ec := echo.New()
+	ec.HideBanner = true
+	ec.Use(middleware.Logger())
+	ec.Use(middleware.Recover())
+	ec.Use(middleware.CORS())
+
+	// WEB FRAMEWORK AUTH
+	ec.POST("/Login", Login)
+	e := ec.Group("")
+	e.Use(middleware.JWT([]byte(config.AUTH_JWT_TOKEN)))
+
+	// WEB FRAMEWORK YÖNLENDİRİCİLERİ
+	router.TodoRouter(e)
+
+	// WEB FRAMEWORK BAŞLAT
+	host := "localhost"
+	port := config.PORT
+	if os.Getenv("PORT") != "" {
+		host = "0.0.0.0"
+		port = os.Getenv("PORT")
+	}
+	ec.Logger.Fatal(ec.Start(host + ":" + port))
+}
